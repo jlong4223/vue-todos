5f525f5e3272357f5d647287573d807dbc8e43a3
Jared Long <jlong4223@gmail.com>
2021-01-14 15:05:49 -0600

    passes props to next child component and styles

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-item">
+    <p>{{ todo.title }}</p>
+  </div>
+</template>
+
+<script>
+// before being able to access props being passed to this component, we need to define them below
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+
+<style scoped>
+.todo-item {
+  color: black;
+  background: whitesmoke;
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,17 +3,23 @@
     <!-- using v-for as a way to loop through the todos array which is defined in props
       defining todo as the parameter which could be named anything
       need v-bind:key for this
+      I am passing props again to the next component
        -->
     <div v-for="todo in todos" v-bind:key="todo.id">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem v-bind:todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
+
 // name is the component name that you want to export
 export default {
   name: "Todos",
+  components: {
+    TodoItem,
+  },
   props: ["todos"],
 };
 </script>
