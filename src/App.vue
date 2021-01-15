9bb74c1977f286d2441f169b2e19d5991394eb8a
Jared Long <jlong4223@gmail.com>
2021-01-14 20:35:14 -0600

    connects to rails api and shows data

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,54 @@
     <!-- use the components here -->
     <!-- v-bind is how you pass data as props -->
     <Todos v-bind:todos="todos" />
+    <Locations v-bind:locations="locations" />
   </div>
 </template>
 
 <script>
 // import components here
 import Todos from "./components/Todos";
+import Locations from "./components/Locations";
 
 export default {
   name: "App",
   // define components here
   components: {
     Todos,
+    Locations,
   },
   // data is essentially the state data
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo one",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo three",
-          completed: false,
-        },
-      ],
-    };
+  data: () => ({
+    error: "",
+    locations: [],
+
+    todos: [
+      {
+        id: 1,
+        title: "Todo one",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Todo two",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Todo three",
+        completed: false,
+      },
+    ],
+  }),
+  // this is how you fetch data from API using ajax
+  mounted() {
+    fetch("https://brewery-advisor-api.herokuapp.com/locations")
+      .then((res) => res.json())
+      .then((result) => {
+        this.locations = result;
+      });
   },
+  methods: {},
 };
 </script>
 
---
+++ src/components/Locations.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="location in locations" v-bind:key="location.id">
+    <h1>{{ location.city }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Locations",
+  props: ["locations"],
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
